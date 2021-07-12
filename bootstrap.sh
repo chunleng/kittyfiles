7a06f7d1699d9bedfd3a9027a2adc076c026ed54
Lim Chun Leng <lim@logly.co.jp>
2021-07-12 10:43:35 +0900

    Enable authentication of sudo via touch id

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,3 +9,7 @@ mkdir -p ${HOME}/.config/kitty
 ln -sfn ${APP_PATH}/kitty.conf ${HOME}/.config/kitty/kitty.conf
 ln -sfn ${APP_PATH}/plugins/vim-kitty-navigator/pass_keys.py ${HOME}/.config/kitty/pass_keys.py
 ln -sfn ${APP_PATH}/plugins/vim-kitty-navigator/neighboring_window.py ${HOME}/.config/kitty/neighboring_window.py
+
+# Enable authentication via touch ID
+grep -q pam_tid.so /etc/pam.d/sudo || \
+  sed "s/\(# sudo: auth account password session\)/\1\nauth       sufficient     pam_tid.so/" /etc/pam.d/sudo | sudo tee /etc/pam.d/sudo
