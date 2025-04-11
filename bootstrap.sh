9f67731d2bc7a4e776b35b4cf591bc5067748d46
Lim Chun Leng <limchunleng@gmail.com>
2025-04-11 23:02:53 +0900

    chore: remove vim-kitty-navigator (gone)

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,7 @@ APP_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 mkdir -p ${HOME}/.config/kitty
 
 ln -sfn ${APP_PATH}/kitty.conf ${HOME}/.config/kitty/kitty.conf
-ln -sfn ${APP_PATH}/plugins/vim-kitty-navigator/pass_keys.py ${HOME}/.config/kitty/pass_keys.py
-ln -sfn ${APP_PATH}/plugins/vim-kitty-navigator/neighboring_window.py ${HOME}/.config/kitty/neighboring_window.py
+ln -sfn ${APP_PATH}/default_session.conf ${HOME}/.config/kitty/default_session.conf
 
 # Enable authentication via touch ID
 grep -q pam_tid.so /etc/pam.d/sudo || \
